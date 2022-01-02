762e81f59c5dc1bb67f8533d5d01e5bc19941cda
lyrics <thomasambetsa@gmail.com>
2022-01-03 00:30:04 +0300

    ajj11

--- account/models.py
+++ account/models.py
@@ -2,17 +2,33 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.gis.db import models as md
+import os
+import random
 
 # Create your models here.
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name,ext = os.path.splitext(base_name)
+    return name, ext
+
+def upload_image_path(instance,filename):
+    new_filename = random.randint(1,999992345677653234)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename,ext = ext)
+    return "thumbnails/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename = final_filename )
+
 
 
 class Agent(md.Model):
     agent_id = models.AutoField(primary_key=True,help_text="agent primary id")
+    agent_profile_picture = models.ImageField(upload_to = upload_image_path,blank = True,null =  True,help_text="The agent picture")
     agent_name = models.CharField(max_length=30,help_text="agent name")
     mobile_number = models.CharField(max_length=10,help_text="agent phone number")
     email = models.EmailField(max_length=50,help_text="agent email")
     approved = models.BooleanField(default=False,help_text="approved")
     address = md.PointField(srid=4326,help_text="agent address")
+
     account_number = models.CharField(max_length=30, help_text="bank account")
 
     def __str__(self):
